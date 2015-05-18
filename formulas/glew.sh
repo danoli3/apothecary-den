d3d4fccdeb33ff6b4c769ff41ff0b796d418f170
Daniel Rosser <danoli3@gmail.com>
2015-05-18 15:30:32 +1000

    Formula Updates

--- formulas/glew.sh
+++ formulas/glew.sh
@@ -88,8 +88,8 @@ function copy() {
 
 	# copy license files
 	rm -rf $1/license # remove any older files if exists
-    mkdir -p $1/license
-    cp -v LICENSE.txt $1/license/
+	mkdir -p $1/license
+	cp -v LICENSE.txt $1/license/
 }
 
 # executed inside the lib src dir
